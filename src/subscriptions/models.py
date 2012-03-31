09c132f2d05c9bc69e428e872e950783b137a377
Diogo Damiani <diogodamiani@gmail.com>
2012-03-31 18:15:15 -0300

    Changing Subscription Model to: allow email blank, use ugettext for translations, and adding CPFValidator.

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,20 +1,23 @@
 # coding=utf-8
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from subscriptions.validators import CPFValidator
 
 # Create your models here.
 class Subscription(models.Model):
-    name = models.CharField('Nome', max_length=100)
-    cpf = models.CharField('CPF', max_length=11, unique=True)
-    email = models.EmailField('E-mail', unique=True)
-    phone = models.CharField('Telefone', max_length=20, blank=True)
-    created_at = models.DateTimeField('Criado em', auto_now_add=True)
-    paid = models.BooleanField('Pago')
+    name = models.CharField(_('Nome'), max_length=100)
+    cpf = models.CharField(_('CPF'), max_length=11, unique=True, validators=[CPFValidator])
+    email = models.EmailField(_('E-mail'), unique=True, blank=True)
+    phone = models.CharField(_('Telefone'), max_length=20, blank=True)
+    created_at = models.DateTimeField(_('Criado em'), auto_now_add=True)
+    paid = models.BooleanField(_('Pago'))
     
     def __unicode__(self):
         return self.name
 
     class Meta:
         ordering = ['created_at']
-        verbose_name = u'Inscrição'
-        verbose_name_plural = u'Inscrições'
+        verbose_name = _(u'Inscrição')
+        verbose_name_plural = _(u'Inscrições')
