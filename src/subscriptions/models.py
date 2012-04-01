f98bf7e86884bc4776465b4ea684e9d1694299d2
Diogo Damiani <diogodamiani@gmail.com>
2012-04-01 09:23:41 -0300

    Chaging Subscription to allow null to email. Updating migrations.

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -9,7 +9,7 @@ from subscriptions.validators import CPFValidator
 class Subscription(models.Model):
     name = models.CharField(_('Nome'), max_length=100)
     cpf = models.CharField(_('CPF'), max_length=11, unique=True, validators=[CPFValidator])
-    email = models.EmailField(_('E-mail'), unique=True, blank=True)
+    email = models.EmailField(_('E-mail'), unique=True, null=True)
     phone = models.CharField(_('Telefone'), max_length=20, blank=True)
     created_at = models.DateTimeField(_('Criado em'), auto_now_add=True)
     paid = models.BooleanField(_('Pago'))
